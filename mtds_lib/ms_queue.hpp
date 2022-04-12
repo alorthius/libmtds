76c3e30418ba289f10166a99839140cd4d01a708
MykhailoSobko <m.t.sobko@gmail.com>
2022-04-12 16:03:23 +0300

    I fix constructor

--- mtds_lib/ms_queue.hpp
+++ mtds_lib/ms_queue.hpp
@@ -5,44 +5,50 @@
 #define MTDSLIB_MS_QUEUE_HPP
 
 #include <atomic>
-#include <cstddef>
+#include <optional>
 
 namespace mtds {
 
 template<typename T>
 class MsQueue {
 public:
-    MsQueue() = default;
-    ~MsQueue();
+    MsQueue();
+    ~MsQueue() = default;  // TODO: implement nontrivial destructor
     MsQueue(const MsQueue&) = delete;
     MsQueue& operator=(const MsQueue&) = delete;
     void enqueue(const T& value);
-    T dequeue();
+    std::optional<T> dequeue();
     [[nodiscard]] size_t size() const { return m_size; }
 private:
     struct TaggedPtr {
-        TaggedPtr() = delete;
 
         uintptr_t m_ptr : 48;
         unsigned short m_tag = 0;
     };
     struct Node {
         T m_value{};
-        TaggedPtr m_next_ptr = nullptr;
+        TaggedPtr m_next_ptr = TaggedPtr{reinterpret_cast<uintptr_t>(nullptr)};
     };
     std::atomic<size_t> m_size = 0;
-    std::atomic<TaggedPtr> m_head_ptr = TaggedPtr{new Node{}};  // dummy node
-    std::atomic<TaggedPtr> m_tail_ptr = m_head_ptr;
+    std::atomic<TaggedPtr> m_head_ptr;
+    std::atomic<TaggedPtr> m_tail_ptr;
 };
 
 template<typename T>
-MsQueue<T>::~MsQueue() {
-    while (m_head_ptr) {
-        auto temp = m_head_ptr;
-        m_head_ptr = m_head_ptr->m_next_ptr;
-        delete temp;
-    }
+MsQueue<T>::MsQueue() {
+    auto temp = reinterpret_cast<uintptr_t>(new Node{});
+    m_head_ptr = TaggedPtr{temp};  // dummy node
+    m_tail_ptr = TaggedPtr{temp};
 }
+
+//template<typename T>
+//MsQueue<T>::~MsQueue() {
+//    while (m_head_ptr) {
+//        auto temp = m_head_ptr;
+//        m_head_ptr = m_head_ptr->m_next_ptr;
+//        delete temp;
+//    }
+//}
 
 }  // namespace mtds
 
