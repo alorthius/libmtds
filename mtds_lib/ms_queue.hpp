73c7922ad852611a25f9e55b9b11589bce5fe584
MykhailoSobko <m.t.sobko@gmail.com>
2022-04-14 15:13:10 +0300

    Add helper functions

--- mtds_lib/ms_queue.hpp
+++ mtds_lib/ms_queue.hpp
@@ -6,49 +6,46 @@
 
 #include <atomic>
 #include <optional>
+#include <bitset>  // TODO: REMOVE
 
 namespace mtds {
+
+namespace details {
+
+inline __attribute__((always_inline)) uintptr_t get_incremented_tag(uintptr_t tagged_ptr) {
+    auto inc_tag =
+            ((0x000000000003ffc & tagged_ptr >> 50) ^ (0x0000000000000003 & tagged_ptr)) + 1;
+    return (0x1110000000000000 & inc_tag << 50) | (0x0000000000000003 & inc_tag)
+           | (0x0003fffffffffffc & tagged_ptr);
+}
+
+template<typename T>
+uintptr_t to_tagged_ptr(T* ptr) {
+    return 0x0003fffffffffffc & reinterpret_cast<uintptr_t>(ptr);
+}
+
+}  // namespace details
 
 template<typename T>
 class MsQueue {
 public:
-    MsQueue();
+    MsQueue() = default;
     ~MsQueue() = default;  // TODO: implement nontrivial destructor
     MsQueue(const MsQueue&) = delete;
     MsQueue& operator=(const MsQueue&) = delete;
     void enqueue(const T& value);
     std::optional<T> dequeue();
     [[nodiscard]] size_t size() const { return m_size; }
-private:
-    struct TaggedPtr {
 
-        uintptr_t m_ptr : 48;
-        unsigned short m_tag = 0;
-    };
+private:
     struct Node {
         T m_value{};
-        TaggedPtr m_next_ptr = TaggedPtr{reinterpret_cast<uintptr_t>(nullptr)};
+        uintptr_t m_next_ptr = reinterpret_cast<uintptr_t>(nullptr);
     };
     std::atomic<size_t> m_size = 0;
-    std::atomic<TaggedPtr> m_head_ptr;
-    std::atomic<TaggedPtr> m_tail_ptr;
+    std::atomic<uintptr_t> m_head_ptr = details::to_tagged_ptr(new Node{});  // dummy node
+    std::atomic<uintptr_t> m_tail_ptr = m_head_ptr.load(std::memory_order_relaxed);
 };
-
-template<typename T>
-MsQueue<T>::MsQueue() {
-    auto temp = reinterpret_cast<uintptr_t>(new Node{});
-    m_head_ptr = TaggedPtr{temp};  // dummy node
-    m_tail_ptr = TaggedPtr{temp};
-}
-
-//template<typename T>
-//MsQueue<T>::~MsQueue() {
-//    while (m_head_ptr) {
-//        auto temp = m_head_ptr;
-//        m_head_ptr = m_head_ptr->m_next_ptr;
-//        delete temp;
-//    }
-//}
 
 }  // namespace mtds
 
